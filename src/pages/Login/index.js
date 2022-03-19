4804d76049a3491abe0f2e5920d01fdb99827d95
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 11:31:53 -0300

    criando tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import {
     Container,
+    AreaLogin,
+    AreaLogo,
+    AreaFormulario,
+    Botao,
+
+    Titulo,
+    Input,
 } from './styles';
 
-export default function Login() {
+export default function Signin() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
     return (
-        <div>
-            <h1>testando pagina login</h1>
-        </div>
+        <Container>
+            <AreaLogin>
+                <AreaLogo>
+                    <Titulo style={{ color: '#fff' }}>Desafio BemPaggo</Titulo>
+                </AreaLogo>
+
+                <AreaFormulario>
+                    <Titulo>Entrar</Titulo>
+                    <Input
+                        type="text"
+                        placeholder='email@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        type="password"
+                        placeholder='*******'
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <Botao type="submit" onClick={handleSubmit}>Acessar</Botao>
+                </AreaFormulario>
+
+                <Link style={{ margin: 20, color: '#000', cursor: 'pointer' }}
+                    to='/cadastrar'>Criar uma conta
+                </Link>
+
+            </AreaLogin>
+        </Container >
     );
 }
+
