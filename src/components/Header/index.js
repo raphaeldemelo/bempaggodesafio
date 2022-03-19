0c3e68704d274931a0345533fcc54d32eb05754c
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 14:31:25 -0300

    consertando header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,50 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
+import { BiLogOutCircle } from 'react-icons/bi';
+import { TiShoppingCart, TiHome } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
 
 import {
     Container,
     Botao,
     TextoBotao,
+    Perfil,
+    Texto,
+    Nome,
+    AreaBotao,
+    TextoMenu,
 } from './styles';
 
 export default function Header() {
 
-    const { signOut } = useContext(AuthContext);
+    const { user, signOut } = useContext(AuthContext);
 
     return (
         <Container>
+
+            <AreaBotao>
+
+                <Link style={{ alignItems: 'center', display: 'flex', marginRight: 10}}>
+                    <TiHome size={20} color='#fff' />
+                    <TextoMenu>Início</TextoMenu>
+                </Link>
+
+                <Link style={{ alignItems: 'center', display: 'flex' }}>
+                    <TiShoppingCart size={20} color='#fff' />
+                    <TextoMenu>Carrinho</TextoMenu>
+                </Link>
+            </AreaBotao>
+
+
+            <Perfil>
+                <Texto>Bem-vindo(a)</Texto>
+                <Nome>{user.nome}</Nome>
+            </Perfil>
+
+
             <Botao onClick={() => signOut()}>
-                <TextoBotao>
-                    Sair
-                </TextoBotao>
+                <BiLogOutCircle size={20} color='#fff' />
+                <TextoBotao>Sair</TextoBotao>
             </Botao>
         </Container>
     );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,15 +8,59 @@ display:flex;
 align-items: center;
 justify-content: flex-end;
 padding-right: 20px;
+
+@media screen and (max-width: 720px){
+    width: 100%;
+    height: auto;
+    position: relative; 
+}
 `;
 
 export const Botao = styled.button`
 border: 0;
-padding: 10px;
+padding: 5px;
+width: 80px;
+height: 50px;
 border-radius: 8px;
 background-color: #0046A0;
+margin-left: 10px;
+display: flex;
+align-items: center;
+justify-content: space-around;
 `;
 
 export const TextoBotao = styled.h2`
 color: #fff;
+font-size: 12px;
+`;
+
+export const Perfil = styled.div`
+line-height: 1;
+margin-right: 10px;
+margin-left: 10px;
+
+@media screen and (max-width: 720px){
+    display: none;
+}
+`;
+
+export const Texto = styled.h5`
+font-weight: lighter;
+`;
+
+export const Nome = styled.h2`
+font-weight: bold;
+`;
+
+export const AreaBotao = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 10px;
+`;
+
+export const TextoMenu = styled.h5`
+font-weight: lighter;
+margin-left: 2px;
+padding: 10px;
 `;
