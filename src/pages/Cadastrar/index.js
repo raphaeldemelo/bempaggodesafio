03ac07dd41363a395965e04a94a13e10adb80956
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 11:31:39 -0300

    criando tela de cadastro

--- src/pages/Cadastrar/index.js
+++ src/pages/Cadastrar/index.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+
 
 import {
     Container,
+    AreaLogin,
+    AreaLogo,
+    AreaFormulario,
+    Botao,
+    Imagem,
+    Titulo,
+    Input,
 } from './styles';
 
-export default function Cadastrar() {
+export default function Signup() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [nome, setNome] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>testando pagina Cadastrar</h1>
-        </div>
+        <Container>
+            <AreaLogin>
+                <AreaLogo>
+                    <Titulo style={{ color: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Desafio BemPaggo</Titulo>
+                </AreaLogo>
+
+                <AreaFormulario>
+                    <Titulo>Cadastrar uma conta</Titulo>
+                    <Input
+                        type="text"
+                        placeholder='seu nome'
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
+                    <Input
+                        type="text"
+                        placeholder='email@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        type="password"
+                        placeholder='*******'
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                    />
+                    <Botao type="submit" onClick={handleSubmit}>Cadastrar</Botao>
+                </AreaFormulario>
+
+                <Link style={{ margin: 20, color: '#000', cursor: 'pointer' }}
+                    to='/'>Já tenho uma conta
+                </Link>
+
+            </AreaLogin>
+        </Container >
     );
 }
+
--- src/pages/Cadastrar/styles.js
+++ src/pages/Cadastrar/styles.js
@@ -1,5 +1,66 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+height: 100%;
+display:flex;
+align-items: center;
+justify-content: center;
+`;
+
+export const AreaLogin = styled.div`
+background-color: #eaeaec;
+width: 600px;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+border-radius: 15px;
+`;
+
+export const AreaLogo = styled.div`
+background-color: #115F9E;
+width: 100%;
+border-top-right-radius: 10px;
+border-top-left-radius: 10px;
+padding: 10px;
+`;
+
+export const Imagem = styled.img`
+padding: 20px;
+width: 170px;
+height: 130px;
 
 `;
+
+export const Titulo = styled.h1`
+text-align: center;
+color: #181c2e;
+`;
+
+export const AreaFormulario = styled.div`
+margin-top: 1.5em;
+width: 90%;
+display: flex;
+flex-direction: column;
+`;
+
+export const Input = styled.input`
+margin-bottom: 15px;
+height: 35px;
+border: 0;
+border-radius: 7px;
+padding: 10px;
+font-size: 15px;
+background-color: #fff;
+`;
+
+export const Botao = styled.button`
+height: 35px;
+border:0;
+border-radius: 7px;
+background-color: #0046A0;
+color: #fff;
+font-weight: bold;
+font-size: 18px;
+`;
+
