0077fe599b0d4a21f70bea01ead61fdbb16946b2
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 13:07:13 -0300

    adicionando toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import AuthProvider from './contexts/auth';
+import { ToastContainer } from 'react-toastify';
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Routes />
       </BrowserRouter>
     </AuthProvider>
