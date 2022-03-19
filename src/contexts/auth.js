c6ffe56d591d3dfdbf36e9946f0ab259eaa6a904
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 13:06:48 -0300

    criando função de logar

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, createContext } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
@@ -25,6 +26,33 @@ export default function AuthProvider({ children }) {
         loadStorage();
     }, [])
 
+    async function signIn(email, senha) {
+        setLoadingAuth(true);
+        await firebase.auth().signInWithEmailAndPassword(email, senha)
+            .then(async (value) => {
+                let uid = value.user.uid;
+
+                const userProfile = await firebase.firestore().collection('usuarios').doc(uid).get();
+
+                let data = {
+                    uid: uid,
+                    nome: userProfile.data().nome,
+                    email: value.user.email,
+                }
+                setUser(data);
+                storageUser(data);
+                setLoadingAuth(false);
+                toast.success('Bem-vindo de volta!');
+
+            })
+            .catch((error) => {
+                console.error(error);
+                setLoadingAuth(false);
+                toast.error('ops...Algo deu errado!!!');
+            })
+    }
+
+
     async function signUp(email, senha, nome) {
         setLoadingAuth(true);
         await firebase.auth().createUserWithEmailAndPassword(email, senha)
@@ -34,7 +62,7 @@ export default function AuthProvider({ children }) {
                 await firebase.firestore().collection('usuarios')
                     .doc(uid).set({
                         nome: nome,
-                        
+
                     })
                     .then(() => {
                         let data = {
@@ -45,12 +73,12 @@ export default function AuthProvider({ children }) {
                         setUser(data);
                         storageUser(data);
                         setLoadingAuth(false);
-                        //toast.success('Bem-vindo a plataforma');
+                        toast.success('Bem-vindo a plataforma');
                     })
             })
             .catch((error) => {
                 console.log(error);
-                //toast.error('ops...Algo deu errado!!!');
+                toast.error('ops...Algo deu errado!!!');
                 setLoadingAuth(false);
             })
     }
@@ -63,7 +91,7 @@ export default function AuthProvider({ children }) {
         await firebase.auth().signOut();
         localStorage.removeItem('DesafioBemPaggo');
         setUser(null);
-        //toast.success('Até breve!');
+        toast.success('Até breve!');
     }
 
     return (
@@ -72,8 +100,10 @@ export default function AuthProvider({ children }) {
                 signed: !!user,
                 user,
                 loading,
+                signIn,
                 signUp,
                 signOut,
+                loadingAuth,
             }}>
             {children}
         </AuthContext.Provider >
