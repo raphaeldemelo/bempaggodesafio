a684b1f648f9017b911165fbe810fb3775194444
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-19 12:28:47 -0300

    criando função de cadastrar e deslogar

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -25,12 +25,55 @@ export default function AuthProvider({ children }) {
         loadStorage();
     }, [])
 
+    async function signUp(email, senha, nome) {
+        setLoadingAuth(true);
+        await firebase.auth().createUserWithEmailAndPassword(email, senha)
+            .then(async (value) => {
+                let uid = value.user.uid;
+
+                await firebase.firestore().collection('usuarios')
+                    .doc(uid).set({
+                        nome: nome,
+                        
+                    })
+                    .then(() => {
+                        let data = {
+                            uid: uid,
+                            nome: nome,
+                            email: value.user.email,
+                        }
+                        setUser(data);
+                        storageUser(data);
+                        setLoadingAuth(false);
+                        //toast.success('Bem-vindo a plataforma');
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+                //toast.error('ops...Algo deu errado!!!');
+                setLoadingAuth(false);
+            })
+    }
+
+    function storageUser(data) {
+        localStorage.setItem('DesafioBemPaggo', JSON.stringify(data))
+    }
+
+    async function signOut() {
+        await firebase.auth().signOut();
+        localStorage.removeItem('DesafioBemPaggo');
+        setUser(null);
+        //toast.success('Até breve!');
+    }
+
     return (
         <AuthContext.Provider
             value={{
                 signed: !!user,
                 user,
                 loading,
+                signUp,
+                signOut,
             }}>
             {children}
         </AuthContext.Provider >
